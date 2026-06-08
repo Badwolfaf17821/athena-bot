ce09a2224e2b07ea444c2948d137c31004e504dc
Badwolfaf17821 <verrecchia.louka@gmail.com>
2026-06-08 18:50:12 +0200

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,17 @@
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"Bot connecté en tant que {bot.user}")
+
+@bot.command()
+async def bonjour(ctx):
+    await ctx.send(f"Salut {ctx.author.name} ! 👋")
+
+bot.run("TOKEN_ICI")
